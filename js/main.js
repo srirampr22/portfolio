6a2636b4a35d4900395bc1c0a96d7a63709d135b
srirampr22 <srirampr@umich.edu>
2024-01-18 15:30:41 -0500

    site update design

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    document.getElementById("navbar").style.top = "0";
+    document.getElementById("navbar").style.top = "0px";
     //document.getElementById("navbar").classList.remove('dark');
     document.getElementById("myBtn").style.display = "block";
   } else {
-    document.getElementById("navbar").style.top = "-50px";
+    document.getElementById("navbar").style.top = "0px";
     document.getElementById("myBtn").style.display = "none";
   }
 }
@@ -14,4 +14,15 @@ function scrollFunction() {
 function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// JavaScript function to toggle project description visibility
+function toggleProjectDescription(id) {
+  var description = document.getElementById("project-description-" + id);
+
+  if (description.style.maxHeight) {
+    description.style.maxHeight = null; // Hide the description
+  } else {
+    description.style.maxHeight = "500px"; // Show the description
+  }
 }
